a0d280b7d960eab6b4e3c638814feef40a7ff22c
Josué <114358135+Castellani-Josue@users.noreply.github.com>
2023-10-02 14:28:36 +0200

    Add files via upload

---
+++ BD_Maraicher/Compile.sh
@@ -0,0 +1,19 @@
+g++ -o CreationBD CreationBD.cpp -I/usr/include/mysql -m64 -L/usr/lib64/mysql -lmysqlclient -lpthread -lz -lm -lrt -lssl -lcrypto -ldl
+
+CreationBD
+
+mysql -u Student -p
+
+PassStudent1_
+
+show databases;
+
+use PourStudent;
+
+show tables;
+
+select * from articles;
+...
+select * from clients;
+
+(autre table vide)
