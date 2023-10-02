eed60ee547a4d216f9d3afb569a5983b29f455a0
Josué <114358135+Castellani-Josue@users.noreply.github.com>
2023-10-02 15:52:53 +0200

    Add files via upload

---
+++ Serveur/OVESP/OVESP.h
@@ -0,0 +1,13 @@
+#ifndef OVESP_H
+#define OVESP_H
+
+#define NB_MAX_CLIENTS 5
+
+
+bool OVESP(char* requete,char* reponse, int socket);
+bool OVESP_Login(const char* user,const char* motDePasse);
+void OVESP_Logout();
+void OVESP_Close();
+
+
+#endif
